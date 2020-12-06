9db39af6806415957522be55e48ad7645631e046
JuliaNastenko <juli.nastenko@gmail.com>
2020-12-07 00:49:34 +0200

    iinitial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const burger = document.getElementById('burger-menu');
+const menu = document.getElementById('mob-menu');
+const main = document.getElementById('main-section');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active-burger');
+  menu.classList.toggle('active-menu');
+  main.classList.toggle('is-blured');
+});
